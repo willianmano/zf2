6948d049e1a7ef3d20b51000410a0115085b9b68
Willian Mano <Willianmanoaraujo@gmail.com>
2012-10-29 20:47:04 -0300

    Album module created

---
+++ module/Album/src/Album/Controller/TesteController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class TesteController extends AbstractActionController
+{
+	public function indexAction()
+	{
+	}
+}
